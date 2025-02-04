5f12404a74e6ea3a6f6f246b75b6ce07c2cd0b85
émile-lenain <emile.lenain@laplateforme.io>
2025-02-04 13:56:27 +0100

    Update recette-idr.php

--- menu/recette-idr.php
+++ menu/recette-idr.php
@@ -36,7 +36,7 @@ $allRecipes = getAllRecipes($pdo);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-    <link rel="stylesheet" href="idriss.css">
+    <link rel="stylesheet" href="https://emile-lenain.students-laplateforme.io/idriss.css">
     <title>Food Website</title>
 </head>
 <body>
@@ -46,7 +46,7 @@ $allRecipes = getAllRecipes($pdo);
         <div class="title"><h1 data-aos="fade-left" data-aos-duration="1000" >Nos meilleures recettes</h1></div>
         <div class="foods">
             <div class="card" data-aos="fade-up" data-aos-duration="1000">
-                <img src="emile-lenain.students-laplateforme.io/images/salad.png" alt="">
+                <img src="https://emile-lenain.students-laplateforme.io/images/salad.png" alt="">
                 <div class="discription">
                     <h1> Red Spinach with Carrot</h1>
                     <p>A spinach salad features tender spinach leaves tossed with vibrant toppings like crisp bacon, sliced mushrooms, and hard-boiled eggs, all drizzled in a zesty vinaigrette, offering a flavorful and nutritious combination in every bite.</p>
@@ -54,7 +54,7 @@ $allRecipes = getAllRecipes($pdo);
                 </div>
             </div>
             <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
-                <img src="emile-lenain.students-laplateforme.io/images/salad1.png" alt="">
+                <img src="https://emile-lenain.students-laplateforme.io/images/salad1.png" alt="">
                 <div class="discription">
                     <h1>Spinach With Carrot</h1>
                     <p>Combining the leafy goodness of spinach with the vibrant crunch of carrots, this salad offers a harmonious balance of flavors and textures, making it a refreshing and healthful choice for any meal and burst of freshness in every mouthful.</p>
@@ -62,7 +62,7 @@ $allRecipes = getAllRecipes($pdo);
                 </div>
             </div>
             <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="400">
-                <img src="emile-lenain.students-laplateforme.io/images/salad2.png" alt="">
+                <img src="https://emile-lenain.students-laplateforme.io/images/salad2.png" alt="">
                 <div class="discription">
                     <h1>Green leave with Tomato</h1>
                     <p>Green leaf and tomato salad: A simple yet vibrant combination of crisp, leafy greens paired with juicy, ripe tomatoes, accented with a tangy dressing, offering a refreshing burst of flavors and creates a delightful salad that's both light and satisfying.</p>
@@ -70,7 +70,7 @@ $allRecipes = getAllRecipes($pdo);
                 </div>
             </div>
             <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
-                <img src="emile-lenain.students-laplateforme.io/images/salad3.png" alt="">
+                <img src="https://emile-lenain.students-laplateforme.io/images/salad3.png" alt="">
                 <div class="discription">
                     <h1>Mixed Salad</h1>
                     <p>A mixed salad is a delightful ensemble of assorted fresh vegetables, fruits, nuts, and sometimes cheese, combined with a flavorful dressing.for making it a satisfying and nutritious option for a wholesome meal or a refreshing side.</p>
@@ -89,7 +89,7 @@ $allRecipes = getAllRecipes($pdo);
                 <div class="card">
                     <?php echo htmlspecialchars($recipe['image'] ?? 'images/default.png'); ?> 
                          <?php echo htmlspecialchars($recipe['name'] ?? 'Recette'); ?>
-                        <img src="emile-lenain.students-laplateforme.io/images/salad.png" alt=""> 
+                        <img src="https://emile-lenain.students-laplateforme.io/images/salad.png" alt=""> 
                     <div class="discription">
                         <h1><?php echo htmlspecialchars($recipe['name'] ?? 'Sans nom'); ?></h1>
                         <p><?php echo htmlspecialchars($recipe['description'] ?? 'Aucune description disponible'); ?></p>
